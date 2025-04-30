5da0a70db4d581f07ae913b91b3ed55f3bbbc358
Zewsic <danilanicishin@gmail.com>
2025-05-01 06:04:27 +1000

    Initial Commit

---
+++ Swiftgram-Test/Swiftgram_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftgram_TestApp.swift
+//  Swiftgram-Test
+//
+//  Created by Zewsic on 01.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Swiftgram_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
